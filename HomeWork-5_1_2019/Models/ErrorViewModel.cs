8702cb85b289e129f1914d81c0913e4465ba4f5f
Mendy <Mendelmullerpupa@gmail.com>
2019-05-01 23:44:10 -0400

    Add project files.

---
+++ HomeWork-5_1_2019/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HomeWork_5_1_2019.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
